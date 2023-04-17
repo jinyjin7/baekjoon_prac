0987d2461a94b6d9a294693249d8175e72bb6df2
sojin park <psjin977@naver.com>
2023-04-17 22:13:56 +0900

    baekjoon prac1-5

---
+++ 1-5prac.py
@@ -0,0 +1,24 @@
+# #1000 덧셈
+A, B = map(int,input().split())
+print(A+B)
+
+#뺄셈
+A, B = map(int,input().split())
+print(A-B)
+
+#곱셈
+A, B = map(int,input().split())
+print(A*B)
+
+#나눗셈
+A, B = map(int,input().split())
+print(A/B)
+
+#사칙연산
+A, B = map(int,input().split())
+print(A+B)
+print(A-B)
+print(A*B)
+print(int(A/B))
+print(A%B)
+
