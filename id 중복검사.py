6cf925cff480815d12d819222ea74d66f1d4be24
sojin park <psjin977@naver.com>
2023-10-05 19:45:34 -0700

    id 유효성검사

---
+++ id 중복검사.py
@@ -0,0 +1,16 @@
+#문제
+#첫째 줄에 준하가 가입하려고 하는 사이트에 이미 존재하는 아이디가 주어진다. 
+#아이디는 알파벳 소문자로만 이루어져 있으며, 길이는 50자를 넘지 않는다.
+
+
+user_new_id = str(input())
+user_id = user_new_id
+
+if user_new_id == user_id :
+    print(user_new_id+"??!")
+    
+
+
+
+
+
