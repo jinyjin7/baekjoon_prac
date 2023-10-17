61f5a4f631b9b0ee7c8621ce74f526f36edcae13
sojin park <psjin977@naver.com>
2023-10-17 00:29:08 -0700

    update 11022,10699

--- 11022.py
+++ 11022.py
@@ -1,3 +1,4 @@
+#11022
 #첫째 줄에 테스트 케이스의 개수 T가 주어진다.
 #각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
 
@@ -8,3 +9,9 @@ n = int(input())
 for i in range(1, n+1):
     A, B = map(int,input().split())
     print(f'Case #{i}: {A} + {B} = {A+B}')
+
+#10699
+#오늘날짜 출력
+
+import datetime
+print(datetime.date.today())
