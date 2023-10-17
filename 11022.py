38e7023a34d4e2da132a71ba7dca3acdb84117ce
sojin park <psjin977@naver.com>
2023-10-17 00:18:39 -0700

    11022

---
+++ 11022.py
@@ -0,0 +1,10 @@
+#첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+#각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+
+n = int(input())
+
+
+for i in range(1, n+1):
+    A, B = map(int,input().split())
+    print(f'Case #{i}: {A} + {B} = {A+B}')
